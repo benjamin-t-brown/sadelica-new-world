492fa6a05c066ae59b4ec2d4acbe50734c85bf22
benjaminbrown <benjamin.t.brown@hotmail.com>
2022-11-29 23:38:43 -0700

    change case 2

---
+++ cpp/test/Logger.test.cpp
@@ -0,0 +1,21 @@
+#include "logger.h"
+#include <gtest/gtest.h>
+
+TEST(LoggerTest, CanSupportMultipleTypesOfLogging) {
+  Logger::disabled = false;
+  logger::info("logger test arg=%s num=%c dub=%d", "arg", 1, 1.);
+  logger::debug("logger test arg=%s num=%c dub=%d", "arg", 1, 1.);
+  logger::warn("logger test arg=%s num=%c dub=%d", "arg", 1, 1.);
+  logger::error("logger test arg=%s num=%c dub=%d", "arg", 1, 1.);
+  Logger() << "SDL2Wrapper Test" << Logger::endl;
+  Logger(LogType::INFO) << "SDL2Wrapper Log Test" << Logger::endl;
+  Logger(LogType::DEBUG) << "SDL2Wrapper Log Test" << Logger::endl;
+  Logger(LogType::WARN) << "SDL2Wrapper Log Test" << Logger::endl;
+  Logger(LogType::ERROR) << "SDL2Wrapper Log Test" << Logger::endl;
+
+  logger::info("Escape percent %%s", "arg");
+  // logger::warn("What?");
+
+  // expect no errors
+  EXPECT_EQ(1, 1);
+}
---
+++ cpp/test/state/ClientState.test.cpp
@@ -0,0 +1,65 @@
+#include "game/in2/in2.h"
+#include "game/stateClient/cliContext.h"
+#include "game/stateClient/cliState.h"
+#include "logger.h"
+#include <fstream>
+#include <gtest/gtest.h>
+
+using namespace snw;
+using namespace snw::state;
+using ClientContext = snw::state::ClientContext;
+
+class ClientStateTest : public testing::Test {
+protected:
+  static void SetUpTestSuite() {
+    in2::init(readIn2CompiledSrcMock());
+    ClientContext::init();
+  }
+  static void TearDownTestSuite() {}
+  void SetUp() override { Logger::disabled = true; }
+  void TearDown() override {}
+
+  static std::string readIn2CompiledSrcMock() {
+    const std::string path = "test/in2/main.compiled.mock.js";
+    Logger(LogType::DEBUG) << "Reading in2 compiled src from " << path
+                           << Logger::endl;
+    const std::ifstream src(path);
+
+    std::stringstream buffer;
+    buffer << src.rdbuf();
+    return buffer.str();
+  }
+
+  static const ClientState& getState() {
+    return ClientContext::get().getState();
+  }
+};
+
+TEST_F(ClientStateTest, CanStartContinueAndStopAConversation) {
+  // a startTalk should show the first line of text in the conversation
+  dispatch::startTalk("main1");
+  ClientContext::get().update();
+  EXPECT_EQ(getState().in2.conversationText, "The value of test is value.");
+  EXPECT_TRUE(helpers::isSectionVisible(getState(), SectionType::CONVERSATION));
+
+  // a blank update shouldn't change the in2 state
+  ClientContext::get().update();
+  EXPECT_EQ(getState().in2.conversationText, "The value of test is value.");
+
+  // continuing the talk should change the text
+  dispatch::continueTalk();
+  ClientContext::get().update();
+  EXPECT_EQ(getState().in2.conversationText,
+            "The value of test is value.\n\nThis node currently has no actual "
+            "content.");
+  EXPECT_TRUE(helpers::isSectionVisible(getState(), SectionType::CONVERSATION));
+
+  // ending a conversation should remove it from the sections
+  dispatch::continueTalk();
+  dispatch::endTalk();
+  ClientContext::get().update();
+  EXPECT_FALSE(
+      helpers::isSectionVisible(getState(), SectionType::CONVERSATION));
+  EXPECT_EQ(getState().in2.waitingState, In2WaitingState::IN2_NONE);
+  EXPECT_FALSE(getCliContext().getIn2Ctx().isExecutionActive());
+}
